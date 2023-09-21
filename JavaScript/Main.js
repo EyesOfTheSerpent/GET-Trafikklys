a3c8caa882985999935dc317d77eb67eec7232f7
Eyes of The Serpent <141904493+EyesOfTheSerpent@users.noreply.github.com>
2023-09-21 14:46:37 +0200

    Pushed all the functions into a class
    All of the functions has been moved into a class as a test project (along with a few trial and errors with setTimeout not working along with this)

--- JavaScript/Main.js
+++ JavaScript/Main.js
@@ -1,45 +1,66 @@
+"use strict";
 //Start
 
 let toggler = false;
 
-function changeLight(colorR, colorY, colorG){
-    let redLight = document.getElementById("redCircle");
-    let yellowLight = document.getElementById("yellowCircle");
-    let greenLight = document.getElementById("greenCircle");
-    redLight.style.backgroundColor = String(colorR);
-    yellowLight.style.backgroundColor = String(colorY);
-    greenLight.style.backgroundColor = String(colorG);
-}
+class Main{
 
-function autoLight(){
-    changeLight('#b71b1b', '', '');
-    setTimeout(changeToRed, 1000)
-}
+    changeLight(colorR, colorY, colorG){
+        let redLight = document.getElementById("redCircle");
+        let yellowLight = document.getElementById("yellowCircle");
+        let greenLight = document.getElementById("greenCircle");
+        redLight.style.backgroundColor = String(colorR);
+        yellowLight.style.backgroundColor = String(colorY);
+        greenLight.style.backgroundColor = String(colorG);
+    }
 
-function changeToRed(){
-    changeLight('#b71b1b', '', '');
-    setTimeout(changeToYellow,3000)
-}
+    async autoLight(){
+        this.changeLight('#b71b1b', '', '');
+        //setTimeout(this.changeToRed, 1000);
+        await delay(1000);
+        this.changeToRed();
+    }
 
-function changeToYellow(){
-    if(toggler == false){
-        changeLight('#b71b1b', '#cdcd1f', '');
-        setTimeout(changeToGreen, 3000)
-        toggler = true;
-    }else{
-        changeLight('', '#cdcd1f', '');
-        setTimeout(changeToRed, 3000)
-        toggler = false;
+    async changeToRed(){
+        this.changeLight('#b71b1b', '', '');
+        //setTimeout(this.changeToYellow,3000);
+        await delay(3000);
+        this.changeToYellow();
+
+    }
+    
+    async changeToYellow(){
+        if(toggler == false){
+            this.changeLight('#b71b1b', '#cdcd1f', '');
+            await delay(3000);
+            this.changeToGreen();
+            //setTimeout(this.changeToGreen, 3000);
+            toggler = true;
+        }else{
+            this.changeLight('', '#cdcd1f', '');
+            await delay(3000);
+            this.changeToRed();
+            //setTimeout(this.changeToRed, 3000);
+            toggler = false;
+        }
+    }
+    
+    async changeToGreen(){
+        this.changeLight('', '', '#34cd1f');
+        await delay(3000);
+        this.changeToYellow();
+        //setTimeout(this.changeToYellow, 3000);
+    }
+    
+    stopLight(){
+        location.reload();
     }
 }
 
-function changeToGreen(){
-    changeLight('', '', '#34cd1f');
-    setTimeout(changeToYellow, 3000)
-}
+let light = new Main();
 
-function stopLight(){
-    location.reload();
+function delay(delayInMs) {
+    return new Promise((resolve) => setTimeout(resolve, delayInMs));
 }
 
 //End
